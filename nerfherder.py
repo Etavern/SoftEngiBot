b7ae0d509a6fa1c932e8266b36bee39f281bc227
Etavern <elijahT1023@gmail.com>
2017-12-12 16:38:06 -0500

    XML ADD BOT

--- nerfherder.py
+++ nerfherder.py
@@ -2,22 +2,46 @@ import bot as nerf
 from xml.dom import minidom
 
 
-def view_bots():
-    # Function to view the existing bots
+def gen_bot_list():
+    # Generate the bot list
 
     bot_list = minidom.parse('bots.xml')
-    bots = bot_list.getElementsByTagName('bot')
-    print("Total Bots: ", len(bots))
+    # bots = bot_list.getElementsByTagName('bot')
+    # print('Total Bots: ', len(bots), '\n')
+    return bot_list
 
+
+def view_bots():
+    # Function to view the existing bots in database
+
+    bots = gen_bot_list().getElementsByTagName('bot')
     for bot in bots:
-        print("ID:", bot.attributes['id'].value)
-        print("IP:", bot.attributes['ip'].value)
-        print("MAC:", bot.attributes['mac'].value)
-        print("OS:", bot.attributes['os'].value)
+        print('Bot', bot.attributes['id'].value)
+        print('--> IP:', bot.attributes['ip'].value)
+        print('--> MAC:', bot.attributes['mac'].value)
+        print('--> OS:', bot.attributes['os'].value)
+        print('-' * 30, '\n')
+
+
+def add_bot():
+    # Add a bot to the database
+
+    bot_list = gen_bot_list()
+    bots = bot_list.getElementsByTagName('bot')
+
+    new_bot = bot_list.createElement('bot')
+    new_bot.setAttribute('id', len(bots))
+    new_bot.setAttribute('ip', '192.168.0.%d' % len(bots))
+    new_bot.setAttribute('mac', 0xffeeddccbbaa)
+    new_bot.setAttribute('os', ("WinShit", len(bots)))
+
+    bot_text = bot_list.createTextNode('bot')
+    new_bot.appendChild(bot_text)
+    bot_list.childNodes[0].appendChild(new_bot)
 
 
 def end():
-    # Terminates the execution.
+    # Terminates the execution
 
     exit(0)
 
@@ -25,7 +49,7 @@ def end():
 def choice_error():
     # Print an error message in case the user selects a wrong action.
 
-    print("Choice does not exist")
+    print('Choice does not exist')
 
 
 def menu():
@@ -34,19 +58,19 @@ def menu():
     # Returns:
         # The choice of the user.
 
-    print("=" * 30 + "\n\t\t\tMENU\n" + "=" * 30)
-    descriptions = ["View bots",
-                    "Add bot",
-                    "View Monitored Devices",
-                    "Take Screen Shot",
-                    "Exfiltrate File",
-                    "Upload Payload",
-                    "Review Stolen Data",
-                    "Exit"]
+    print('=' * 30 + '\n\t\t\tMENU\n' + '=' * 30)
+    descriptions = ['View bots',
+                    'Add bot',
+                    'View Monitored Devices',
+                    'Take Screen Shot',
+                    'Exfiltrate File',
+                    'Upload Payload',
+                    'Review Stolen Data',
+                    'Exit']
     for num, func in enumerate(descriptions):
-        print("[-->", num, func)
+        print('[-->', num, func)
 
-    choice = input(">>> ")
+    choice = input('>>> ')
     return choice
 
 
@@ -57,22 +81,22 @@ while True:
         view_bots()
 
     elif select == 1:
-        print("function 1")
+        add_bot()
 
     elif select == 2:
-        print("function 2")
+        print('function 2')
 
     elif select == 3:
-        print("function 3")
+        print('function 3')
 
     elif select == 4:
-        print("function 4")
+        print('function 4')
 
     elif select == 5:
-        print("function 5")
+        print('function 5')
 
     elif select == 6:
-        print("function 6")
+        print('function 6')
 
     elif select == 7:
         end()
